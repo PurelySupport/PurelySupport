19d28b01754ed5a110ce26b7dcd91cfac9238865
mhutchison90 <michaelhutchison90@gmail.com>
2017-11-21 14:35:32 -0700

    fixed add user diseases and groups

--- db/update_user_groups.sql
+++ db/update_user_groups.sql
@@ -8,7 +8,7 @@ DECLARE
   groups_array_len int = array_upper(groups_array, 1);
 
  begin
- DELETE FROM usersgroupsusersgroups
+ DELETE FROM usersgroups
 WHERE userid = $1;
 raise notice 'length is %', groups_array_len;
   for i in 1..groups_array_len
