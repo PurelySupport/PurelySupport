1388b207214e0cea80215a233d74495e0d158b55
mhutchison90 <michaelhutchison90@gmail.com>
2017-11-27 11:51:33 -0700

    tested get conversation end point and made a few changes to get it working.

--- server/server.js
+++ server/server.js
@@ -145,7 +145,8 @@ app.put('/api/deletemessage/:id', userController.deleteMessage);
 
 app.get('/api/allposts', userController.getAllPosts);
 
-app.get('/api/getconversation/:id', userController.getConversation);
+app.get('/api/getconversation/:active_user_id/:friend_user_id', userController.getConversation);
+
 // app.post('/api/payment', function (req, res, next) {
 //   //convert amount to pennies
 //   const amountArray = req.body.amount.toString().split('');
