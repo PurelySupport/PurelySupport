1b18f868a94bba645d126f195d21c7015aee7b00
Braydon Squire <guitarf1@hotmail.com>
2017-11-29 09:18:56 -0700

    updated unit tests

--- src/utilities/unitTests.test.js
+++ src/utilities/unitTests.test.js
@@ -4,7 +4,7 @@ test('Group page should grab all posts on componentDidMount', () => {
     expect.assertions(1);
     const url = 'http://localhost:3001/api/allposts'
     return fns.getPosts(url).then( res => {
-        expect(res[1].postid).toEqual(2);
+        expect(res[1].postid).toEqual(3);
     })
 })
 
@@ -20,6 +20,6 @@ test('Post page should grab posts comments on componentDidMount',() => {
     expect.assertions(1);
     const url = 'http://localhost:3001/api/getcomments/2'
     return fns.getComments(url).then( res => {
-        expect(res[0].commentid).toEqual(4);
+        expect(res[0].commentid).toEqual(2);
     })
-})
+}) 
