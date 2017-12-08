3cf722f09d86b15830d0eb83459261b4e3a6642f
jacobcarey93 <31251057+jacobcarey93@users.noreply.github.com>
2017-12-07 17:29:08 -0700

    styled dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -58,53 +58,75 @@ class Dashboard extends Component {
                 <Navbar />
 
 
-                <div className='events'>
-                    {this.props.allEvents.map(event => {
-                        return (
-                            <div className='event'>
-                                <div className='img-holder'><img src={event.image} /></div>
-                                <div className='holder'>
-                                    <Link to={`/events`}><div className='groupName'>{event.groupname}</div></Link>
-                                    <div className='name'>{event.name}</div>
-                                    <div className='date'>{event.date}</div>
-                                    <div className='place'>{`${event.city}, ${event.state}`}</div>
-                                    <div className='time'>{event.starttime}</div>
-                                </div>
-                                <div className='description'>{event.description}</div>
-                            </div>
-                        )
-                    })}
 
-                    {this.state.posts.map(post => {
-                        return (
-                            <div>
-                                {post.image}
-                                {post.title}
-                            </div>
-                        )
-                    })}
+                <div className='main-grid'>
 
-                    <div className='stuff'>
+                    {/* events */}
 
-                        My Interests:
-                    {this.state.interests !== null ? this.state.interests.map(interest => {
-                            return (
-                                <div>{interest.interest_name}</div>
-                            )
-                        }) : null}
+                    <div className='events'>
+                        <div className='events-header'>Events</div>
+                        <div className='event-holder'>
 
-                        {/* {this.state.groups !== null ? this.state.groups.map(group => {
-                            return (
-                                <div>{group.group_name}</div>
-                            )
-                        }) : null} */}
+                            {this.props.allEvents.map(event => {
+                                return (
+                                    <Link to={`/events`} className='fix-link'>
+                                        <div className='event'>
+                                            <div className='img-holder'
+                                                style={{
+                                                    backgroundImage: `url(${event.image})`,
+                                                    backgroundSize: 'cover',
+                                                    backgroundRepeat: 'no-repeat',
+                                                    backgroundPosition: '50% 50%',
+                                                }}>
 
-                        {this.state.diseases !== null ? this.state.diseases.map(disease => {
-                            return (
-                                <div>{disease.disease_name}</div>
-                            )
-                        }) : null}
+                                            </div>
+                                            <div className='holder'>
+                                                <div className='groupName'>{event.groupname}</div>
+                                                <div className='name'>{event.name}</div>
+                                                <div className='date'>{event.date}</div>
+                                                <div className='place'>{`${event.city}, ${event.state}`}</div>
+                                                <div className='time'>{event.starttime}</div>
+                                            </div>
+                                            <div className='description'>{event.description.substr(0, 75) + "..."}</div>
+                                        </div>
+                                    </Link>
+                                )
+                            })}
 
+                        </div>
+
+
+
+                        {/* posts */}
+                        <div className='events-header'>Posts</div>
+                        <div className='event-holder'>
+                            {this.state.posts.map(post => {
+                                return (
+                                    <div className='event'>
+                                        <div className='img-holder'
+                                            style={{
+                                                backgroundImage: `url(${post.image1})`,
+                                                backgroundSize: 'cover',
+                                                backgroundRepeat: 'no-repeat',
+                                                backgroundPosition: '50% 50%',
+                                            }}>
+
+                                        </div>
+                                        <div className='holder'>
+                                            <Link to={`/grouppage`}>
+                                                <div className='groupName'>{post.groupname}</div>
+                                            </Link>
+                                            <Link to={`/postpage/${post.postid}`} className='fix-link'>
+                                                <div className='name'>{post.title}</div>
+                                            </Link>
+                                            <div className='date'>{post.timestamp}</div>
+                                        </div>
+                                        <div className='description' dangerouslySetInnerHTML={{ __html: post.content.substr(0, 50) + "..." }} />
+                                    </div>
+                                )
+                            })}
+
+                        </div>
                     </div>
                 </div>
             </div>
